2f50591941f9dc332334c479ccdefa84056c4346
Petar Sakotic <>
2024-03-27 10:09:21 +0100

    - First commit

---
+++ myCar/myCar/myCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCarApp.swift
+//  myCar
+//
+//  Created by Petar Sakotic on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct myCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
